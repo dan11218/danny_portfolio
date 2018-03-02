bfa39de58a0fd635c5d2be6bfdb1a667c8655615
Daniel Graugnard <dangraug@me.com>
2018-03-02 16:42:18 -0500

    fixes footer media query

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,15 +4,15 @@ const Footer = () => {
   return(
     <footer>
       <div className='container text-center'>
-        <div className='row justify-content-md-center'>
-          <div className='col col-1'>
-            <a href='https://www.github.com/dan11218'><i className='fa fa-github rounded-circle text-light'  style={{fontSize: 55}}></i></a>
+        <div className='row justify-content-center'>
+          <div id='contact-buttons' className='col-auto'>
+            <a href='https://www.github.com/dan11218'><i className='fa fa-github rounded-circle text-light'></i></a>
           </div>
-          <div className='col col-1'>
-            <a href='https://www.linkedin.com/in/danielgraugnard'><i className='fa fa-linkedin text-light' style={{fontSize: 55}}></i></a>
+          <div id='contact-buttons' className='col-auto'>
+            <a href='https://www.linkedin.com/in/danielgraugnard'><i className='fa fa-linkedin text-light'></i></a>
           </div>
-          <div className='col col-1'>
-            <a href='mailto:dangraug@me.com'><i className='fa fa-envelope text-light' style={{fontSize: 55}}></i></a>
+          <div id='contact-buttons' className='col-auto'>
+            <a href='mailto:dangraug@me.com'><i className='fa fa-envelope text-light'></i></a>
           </div>
         </div>
         <p id='copywrite'>Daniel Graugnard © 2017</p>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const HomePage = (props) => {
 	return (
 		<div id='jumbotron' className='jumbotron text-white text-center'>
 			<h1 className='display-4'>It all starts with your idea.</h1><br />
-			<a id='email-button' className='btn btn-outline-light' role='button' alt='available for hire' href='mailto:dangraug@me.com'>AVAILABLE FOR HIRE</a>
+			<a id='hire-button' className='btn btn-outline-light' role='button' alt='available for hire' href='mailto:dangraug@me.com'>AVAILABLE FOR HIRE</a>
 		</div>
 	)
 }
