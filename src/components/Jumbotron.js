191b5c4a4004c4a17cddac39063a3818e6b3c039
Daniel Graugnard <dangraug@me.com>
2018-03-05 12:34:27 -0500

    changes slogan

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -5,7 +5,7 @@ const Jumbotron = (props) => {
 	return (
 		<div id='jumbotron' className='jumbotron text-white text-center'>
 			<h1 id='slogan' className='display-4'>It all starts with your idea.</h1>
-			<p className='lead'>Leave it to a pro. I'm a programmer with a knack for design and user experience.</p>
+			<p className='lead'>Leave it to a pro. I'm a web developer with a knack for design and user experience.</p>
 			<a id='hire-button' className='btn btn-outline-light' role='button' alt='available for hire' href='mailto:dangraug@me.com'>Available for hire</a>
 		</div>
 	)
