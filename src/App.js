736e1469585f34c89b15d2da936e7ba38cb9a2d7
Danny Graugnard <dangraug@me.com>
2019-03-12 16:19:48 -0400

    disables projects section

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Jumbotron from './components/Jumbotron';
 import Footer from './components/Footer'
 import About from './components/About';
 // import Quotes from './components/Quotes'
-import Projects from './components/Projects';
+// import Projects from './components/Projects';
 // import Contact from './components/Contact';
 // import Resume from './components/Resume';
 
