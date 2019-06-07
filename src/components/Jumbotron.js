e8cb4c766e5320aa90f0106394aab707026ed0d2
Danny Graugnard <dangraug@me.com>
2019-06-07 13:54:53 -0400

    updates jumbotron

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -6,7 +6,7 @@ const Jumbotron = (props) => {
 		<div id='jumbotron' className='jumbotron text-white text-center animated fadeIn'>
 			<img id="logo" className="img-fluid" alt='Daniel Graugnard' height="400px" width="600px" src={require('../images/logo-line.png')} />
 			<h1 id='slogan' className='display-4'>It all starts with an idea.</h1>
-			<p className='lead'>I'm a web developer with a knack for design.</p>
+			<p className='lead'>I'm a Software Engineer with a knack for problem-solving.</p>
 			<a id='hire-button' className='btn btn-outline-light' role='button' alt='available for hire' href='mailto:dangraug@me.com'>Available for hire</a>
 		</div>
 	)
